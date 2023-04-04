f2b9e55e656bed82aeebb4f566615ada65b5b968
zmaji <502241@student.saxion.nl>
2023-04-04 14:28:06 +0200

    wip

--- app/app.js
+++ app/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const xlsx = require('xlsx');
+
 const app = express();
 
 app.get('/', (req, res) => {
+  // Read Excel file
+  const workbook = xlsx.readFile('C:\Users\Maurice\Desktop\SSS-dev\webapplication-stichting-memento-mori\app\files\2021-StMM-Overzicht-Weezenkerkhof.xlsx');
+  const sheetNames = workbook.SheetNames;
+  const excelData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetNames[0]]);
+  console.log(excelData);
   res.send('HALLO DIT IS EEN WEBAPPLICATIE!!!!');
+  res.send(`DATA: ${excelData}`);
 });
 
 app.listen(3000, () => {
