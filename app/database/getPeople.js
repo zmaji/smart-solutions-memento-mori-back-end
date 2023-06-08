dcb81bb48bad329659dab18566df48b52549b079
zmaji <502241@student.saxion.nl>
2023-06-08 13:19:17 +0200

    wip

--- app/database/getPeople.js
+++ app/database/getPeople.js
@@ -2,7 +2,22 @@ async function getPeople(client, tableName, searchText, categories) {
   let queryText = `SELECT * FROM ${tableName}`;
 
   if (searchText) {
-    queryText += ` WHERE roepnaam LIKE '%${searchText}%'`;
+    queryText += `
+    WHERE roepnaam ILIKE '%${searchText}%'
+    OR voornamen ILIKE '%${searchText}%'
+    OR achternaam ILIKE '%${searchText}%'
+    OR geboorteplaats ILIKE '%${searchText}%'
+    OR provincie ILIKE '%${searchText}%'
+    OR rol ILIKE '%${searchText}%'
+    OR functie ILIKE '%${searchText}%'
+    OR grave_id ILIKE '%${searchText}%'
+    OR locatie ILIKE '%${searchText}%'
+    OR voorheen_pupil ILIKE '%${searchText}%'
+    OR vader ILIKE '%${searchText}%'
+    OR moeder ILIKE '%${searchText}%'
+    OR opleiding ILIKE '%${searchText}%'
+    OR gehuwd_met ILIKE '%${searchText}%'
+    `;
   }
   
   if (categories) {
